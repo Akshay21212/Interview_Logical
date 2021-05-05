1745fd8017321dc5117fbbb1b86d317cb11233c2
LENOVO <LENOVO@LAPTOP-2BM44HDP>
2021-05-05 14:34:16 +0530

    Logical programs Added

---
+++ LogicalPractice/src/com/cjc/VowelConsonantCount.java
@@ -0,0 +1,30 @@
+package com.cjc;
+
+public class VowelConsonantCount {
+public static void main(String[] args) {
+	String str="Sandeep";
+	countVowels(str);
+}
+
+private static void countVowels(String str) {
+	int vowelCount=0;
+	int consonantCount=0;
+for(int i=0;i<str.length(); i++){
+	if(isVowel(str.charAt(i))) {
+		vowelCount++;
+	}else {
+		consonantCount++;
+	}
+}
+	System.out.println("Vowel Count is  : "+vowelCount);
+	System.out.println("Consonant Count is :"+consonantCount);
+}
+	
+
+
+private static boolean isVowel(char ch) {
+	ch=Character.toUpperCase(ch);
+	
+	return (ch=='A' || ch=='E'|| ch=='I' || ch=='O' ||  ch=='U' );
+}
+}
